9cee973932312cfc6dcaecf29010e9be55e2fd54
NagyBz <nagybz66@gmail.com>
2020-01-13 23:27:16 +0100

    excersice completed

--- Vidly/Models/Movie.cs
+++ Vidly/Models/Movie.cs
@@ -10,14 +10,27 @@ namespace Vidly.Models
     public class Movie
     {
         public int Id { get; set; }
-        [Required]
-        [StringLength(255)]
+
+        
         public Genre Genre { get; set; }
 
+        [Display(Name = "Genre")]
+        [Required]
         public byte GenreId { get; set; }
+
+        [Required]
+        [StringLength(255)]
         public string Name { get; set; }
+
+        
+        [Display(Name = "Release Date")]
         public DateTime ReleaseDate { get; set; }
+
+
         public DateTime DateAdded { get; set; }
+
+       
+        [Display(Name = "Number In Stock")]
         public int NumberInStock { get; set; }
     }
 }
--- Vidly/ViewModels/MovieViewModel.cs
+++ Vidly/ViewModels/MovieViewModel.cs
@@ -11,5 +11,6 @@ namespace Vidly.ViewModels
     {
         public Movie Movie { get; set; }
         public List<Customer> Customers { get; set; }
+       
     }
 }
